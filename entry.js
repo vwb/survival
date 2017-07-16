9841f6079331a5d0a2e4e38b86a33d43d601ca01
Jon Kalfayan <jonk1993@gmail.com>
2017-07-15 18:57:11 -0700

    create basic ui, add entry points using h and j keys for multiplayer

--- entry.js
+++ entry.js
@@ -15,20 +15,32 @@ var infoEl = document.getElementById("info")
 var footerEl = document.getElementById("footer")
 var newGame = document.getElementById("new-game")
 var toolTip = document.getElementById("tooltip")
+var multiplayer = document.getElementById("multiplayer")
 
 
-el.addEventListener("keydown", function(){
+el.addEventListener("keydown", function() {
 
-	if (event.which === 32 && !gameView.inProgress){
-
-		infoEl.className = "info-wrapper center group gone"
-		footerEl.className = "footer visible "
-		canvasEl.className = "visible fade-in"
-		newGame.className = "info gone"
-		toolTip.className = "gone"
-
-
+	if (event.which === 32 && !gameView.inProgress) {
+        //standard game
+        removeMenu();
 		gameView.start(ctx);
-	}
-})
+	} else if (event.which === 72 && !gameView.inProgress) {
+        //player wants to host a multiplayer game, register websockets here
+        removeMenu();
+        gameView.start(ctx);
+    } else if (event.which === 74 && !gameView.inProgress) {
+        //player wants to join an existing game, register websockets here
+        removeMenu();
+        gameView.start(ctx);
+    }
+});
+
+function removeMenu() {
+    infoEl.className = "info-wrapper center group gone"
+    footerEl.className = "footer visible "
+    canvasEl.className = "visible fade-in"
+    newGame.className = "info gone"
+    multiplayer.className = "info gone"
+    toolTip.className = "gone"
+}
 
